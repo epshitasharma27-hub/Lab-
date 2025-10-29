cc69e9bb7e016f0d140f38c07f50487ba3f81e74
Epshita Sharma <epshitasharma27@gmail.com>
2025-10-29 11:09:44 +0530

    Add files via upload

---
+++ fun1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void printDetails();
+
+int main (){
+    printDetails();
+    return 0;
+}
+
+void printDetails(){
+printf("Name : Epshita Sharma \n");
+printf("UNI : UPES \n");
+}
