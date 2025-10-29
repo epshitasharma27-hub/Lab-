86621edfd5b872aa46af02f1f410a8fa4c5ca6f1
Epshita Sharma <epshitasharma27@gmail.com>
2025-10-29 11:10:30 +0530

    Add files via upload

---
+++ fun2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int add( int a , int b );
+int main(){
+    int num1 , num2 , sum ;
+
+    printf("Enter first number: ");
+    scanf("%d" , &num1);
+
+    printf("Enter second number:");
+    scanf("%d" , &num2);
+
+    sum = add(num1 , num2);
+
+    printf("The sum is %d \n" , sum);
+
+    return 0;
+
+}
+
+int add( int a , int b){
+    return a + b;
+}
